f84f80d740a43b1cf1c9dadcc39b8aa5b74c0bb7
Abubakar usman Lafiagi <lafiagi002@gmail.com>
2024-09-01 16:37:48 +0100

    completed

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let drinks = ["Water", "Coke", "Fanta", "Sprite", "Fearless"];
 
 let choice = prompt(user + ", welcome to our restaurant. What would you like to have? Drinks or Food");
 
-if (choice.toLowerCase() === "drinks") {
+if (choice.toLowerCase() ==="drinks") {
     let drinkChoice = prompt("Nice choice! What drink would you like? ");
     alert("Your order " + drinkChoice+ " Has been recived, Arriving in 45mint");
 } else if (choice.toLowerCase() === "food") {
