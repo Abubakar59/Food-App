02cd1b4ad9506f4f9d6ca04bf0749862bd76a450
Abubakar usman Lafiagi <lafiagi002@gmail.com>
2024-09-07 07:54:02 +0100

    good

--- script.js
+++ script.js
@@ -5,15 +5,26 @@ let user = prompt("What is your name?");
 let food = ["Rice", "Beans", "Jollof rice", "Roasted chicken"];
 let drinks = ["Water", "Coke", "Fanta", "Sprite", "Fearless"];
 
-let choice = prompt(user + ", welcome to our restaurant. What would you like to have? Drinks or Food");
+let choice = prompt(user + ", welcome to our restaurant. What would you like to have? Drinks or Food?");
 
-if (choice.toLowerCase() =="drinks") {
-    let drinkChoice = prompt("Nice choice! What drink would you like? ");
-    alert("Your order " + drinkChoice+ " Has been recived, Arriving in 45mint");
-} else if (choice.toLowerCase() =="food") {
-    let foodChoice = prompt("Great! What food would you like? " );
-    alert("Your order" + foodChoice + " has been recived, Arriving soon");
+if (choice.toLowerCase() === "drinks") {
+    let drinkChoice = prompt("Nice choice! We have: " + drinks.join(", ") + ". What drink would you like?");
+    
+    if (drinks.includes(drinkChoice)) {
+        alert("Your order " + drinkChoice + " has been received, arriving in 45 minutes.");
+    } else {
+        alert("Sorry, we don't have that drink.");
+    }
+
+} else if (choice.toLowerCase() === "food") {
+    let foodChoice = prompt("Great! We have: " + food.join(", ") + ". What food would you like?");
+    
+    if (food.includes(foodChoice)) {
+        alert("Your order " + foodChoice + " has been received, arriving soon.");
+    } else {
+        alert("Sorry, we don't have that food.");
+    }
+
 } else {
-    alert("Please select either Food or Drinks.");
+    alert("Please select either 'Food' or 'Drinks'.");
 }
-
